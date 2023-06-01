d26a71ec7db8297153ec19284a2cacb4144700e2
Yousafnlp <132944422+Yousafnlp@users.noreply.github.com>
2023-06-01 15:39:27 -0700

    Add files via upload

---
+++ khaadi.js
@@ -0,0 +1,17 @@
+            var swiper = new Swiper(".mySwipercover", {
+            centeredSlides: true,
+            autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+                  },
+            
+            });
+
+            var swiper = new Swiper(".mySwipersecondary", {
+                slidesPerView: 2,
+                spaceBetween: 10,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                          },
+                          });
